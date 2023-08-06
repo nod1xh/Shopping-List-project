69e64105c75cb3034098a0216858356aa6f8e7cd
nod1xh <Elmedin_Hodzic@outlook.com>
2023-08-06 10:59:14 +0200

    New commit = removing the items from the list

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
+// Funkcija za dodavanje itema u listu
 function addItem(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
 
-  // Validate Input
+  // Alert - ako nema nista u 'Enter Item'
   if (newItem === "") {
     alert("Please add an item");
     return;
   }
 
-  // Create list item
+  // Kreiranje itema u listi
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
 
@@ -25,6 +27,7 @@ function addItem(e) {
   itemInput.value = "";
 }
 
+// Funkcija za kreiranje buttona
 function createButton(classes) {
   const button = document.createElement("button");
   button.className = classes;
@@ -33,11 +36,29 @@ function createButton(classes) {
   return button;
 }
 
+// Funkcija za kreiranje ikonice 'x'
 function createIcon(classes) {
   const icon = document.createElement("i");
   icon.className = classes;
   return icon;
 }
 
+// FUNKCIJE ZA BRISANJE ITEMA IZ LISTE
+// Clearing the items with a 'x' button on list item
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+// Clearing the items with a 'Clear All' button
+function clearItems(e) {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
+
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
